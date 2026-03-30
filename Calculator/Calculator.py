3416681b52470efddd92e50235e2e6077696352a
CJarmaine <caruljames@gmail.com>
2026-03-30 20:39:36 +1030

    All basic functions added

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,3 +1,4 @@
+## Input function - Asks user question - Splits awnser into 3 bits n1, func, n2
 input_str = input("Hello, What would you like me to calculate? ").split()
 
 n1 = int(input_str[0])
@@ -6,8 +7,14 @@ n2 =  int(input_str[2])
 
 ## functions
 
-if func == '+' :
+if func == '+' or 'plus':
     x = n1 + n2
+elif func == '-' or 'minus' :
+    x = n1 - n2
+elif func == '*' or 'multiply':
+    x = n1 * n2
+elif func == '/' or 'divide':
+    x = n1 / n2
 else:
     print("Error")
 print(x)
