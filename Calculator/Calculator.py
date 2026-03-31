debce032cc488b3302d3adff5c281890c70caccd
CJarmaine <caruljames@gmail.com>
2026-03-31 19:24:42 +1030

    Functions made into functions using def, int and float checks, looped

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,20 +1,38 @@
 ## Input function - Asks user question - Splits awnser into 3 bits n1, func, n2
 input_str = input("Hello, What would you like me to calculate? ").split()
 
-n1 = int(input_str[0])
-func = input_str[1]
-n2 =  int(input_str[2])
+while input_str != "quit":
+    n1 = float(input_str[0])
+    func = input_str[1]
+    n2 = float(input_str[2])
+ 
+    ## functions
+    ## Addition
+    def add_numbers(n1,n2):
+        return n1+n2
+    def sub_numbers(n1,n2):
+        return n1-n2
+    def mult_numbers(n1,n2):
+        return n1*n2
+    def div_numbers(n1,n2):
+        return n1/n2
 
-## functions
+    if func in ['+', 'plus']:
+        ans = add_numbers(n1,n2)
+    elif func in ['-', 'minus']:
+        ans = sub_numbers(n1,n2)
+    elif func in ['*', 'multiply']:
+        ans = mult_numbers(n1,n2)
+    elif func in ['/', 'divide']:
+        if n2 == 0:
+            ans = 0
+        else:
+            ans = div_numbers(n1,n2)
+    else:
+        print("Error")
 
-if func == '+' or 'plus':
-    x = n1 + n2
-elif func == '-' or 'minus' :
-    x = n1 - n2
-elif func == '*' or 'multiply':
-    x = n1 * n2
-elif func == '/' or 'divide':
-    x = n1 / n2
-else:
-    print("Error")
-print(x)
+    if ans.is_integer():
+        print(int(ans))
+    else:
+        print(ans)
+    input_str = input("Hello, What would you like me to calculate? ").split()
